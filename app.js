1659b2d61daebe9ee2a18b3a47d761a4b9c7e2c8
Oli <palmbusiness12@gmail.com>
2026-01-28 19:17:17 +0000

    Fully functional simple working calculator! Poorly coded but first finished project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let number1;
-let number2;
-let operation = "";
+let preOperationNumber = 0;
+let postOperationNumber = 0;
+let currentOperation = "";
 
 function add(num1, num2) {
     return num1 + num2;
@@ -28,5 +28,61 @@ function operate(num1, num2, operator) {
             return multiply(num1, num2);
         case "division":
             return divide(num1, num2);
+    }
+}
+
+function click_handler(id) {
+    if (id.includes("number")) {
+        if (currentOperation === "") {
+            if (preOperationNumber === 0) {
+                preOperationNumber = Number(id.substring(7));
+
+                document.getElementById("answer-display").innerText =
+                    preOperationNumber;
+            } else if (preOperationNumber > 0) {
+                let latestNumberClicked = Number(id.substring(7));
+                let newNumber =
+                    String(preOperationNumber) + String(latestNumberClicked);
+
+                document.getElementById("answer-display").innerText = newNumber;
+
+                preOperationNumber = Number(newNumber);
+            }
+        } else {
+            if (postOperationNumber === 0) {
+                postOperationNumber = Number(id.substring(7));
+
+                document.getElementById("answer-display").innerText =
+                    postOperationNumber;
+            } else if (postOperationNumber > 0) {
+                let latestNumberClicked = Number(id.substring(7));
+                let newNumber =
+                    String(postOperationNumber) + String(latestNumberClicked);
+
+                document.getElementById("answer-display").innerText = newNumber;
+
+                postOperationNumber = Number(newNumber);
+            }
+        }
+    } else if (id.includes("equals")) {
+        let total = operate(
+            preOperationNumber,
+            postOperationNumber,
+            currentOperation,
+        );
+
+        document.getElementById("answer-display").innerText = total;
+
+        preOperationNumber = 0;
+        postOperationNumber = 0;
+        currentOperation = "";
+    } else if (
+        id === "addition" ||
+        id === "subtraction" ||
+        id === "multiplication" ||
+        id === "division"
+    ) {
+        currentOperation = id;
+        document.getElementById("answer-display").innerText = "0";
     }
 }
